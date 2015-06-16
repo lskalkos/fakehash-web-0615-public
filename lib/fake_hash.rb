5f12bea2c7882c0937b90a56493df929eb54913c
Lia Skalkos <hello@liaskalkos.net>
2015-06-16 15:05:58 -0400

    Complete

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,33 @@
-class FakeHashWrapper
+class FakeHashWrapper < Hash
 
   # write code here
+
+  def initialize
+    @hash = Hash.new
+  end
+
+  def []=(key, value)
+    new_key = key.is_a?(Symbol) ? key : key.to_sym
+    @hash[new_key] = value
+  end
+
+  def [](key)
+    new_key = key.is_a?(Symbol) ? key : key.to_sym
+    @hash[new_key]
+  end
+
+  def keys
+    @hash.keys.sort!{|x,y| y <=> x }
+  end
+
+  def first_letter(letter)
+    @hash.each_with_object([]) do |(key, value), array|
+      array << value if key[0] == letter
+    end
+  end
+
+  def each
+    @hash.each{|key, value| yield key, value}
+  end
 
 end
